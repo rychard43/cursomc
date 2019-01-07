159feb3da9eb36b7cab44e0a7eff242b61e0d74f
Rychard souza <rychard34@outlook.com>
2019-01-07 19:57:12 -0300

    PUT, DELETE E GET para cliente

---
+++ src/main/java/com/rychard/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,62 @@
+package com.rychard.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.rychard.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(min=5,max=120,message="o temanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchmento Obrigatorio")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+}
--- src/main/java/com/rychard/cursomc/services/ClienteService.java
+++ src/main/java/com/rychard/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.rychard.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rychard.cursomc.domain.Cliente;
+import com.rychard.cursomc.dto.ClienteDTO;
 import com.rychard.cursomc.repositories.ClienteRepository;
+import com.rychard.cursomc.services.exceptions.DataIntegrityException;
 import com.rychard.cursomc.services.exceptions.ObjectNotFundException;
 
 @Service
@@ -20,5 +28,39 @@ public class ClienteService {
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
 		}
+	
+	public Cliente update(Cliente obj) {
+		
+		Cliente newObj = find(obj.getId());//verificar se existe 
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir pq há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
 }
