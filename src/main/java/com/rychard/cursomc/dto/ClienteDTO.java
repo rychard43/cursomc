49a16d9624721f2161b9d5e6d515c68f53da1e3b
Rychard souza <rychard34@outlook.com>
2019-01-09 10:38:52 -0300

    validação customizada, email não repetido na atualização de cliente

--- src/main/java/com/rychard/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rychard/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rychard.cursomc.domain.Cliente;
+import com.rychard.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
