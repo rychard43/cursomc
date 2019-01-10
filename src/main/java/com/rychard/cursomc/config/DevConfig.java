b17e4a082528160714a1842e330453a29a170486
Rychard souza <rychard34@outlook.com>
2019-01-10 19:30:07 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/rychard/cursomc/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.rychard.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rychard.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false; //verificar se tem create no properties
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}	
+	
+}
