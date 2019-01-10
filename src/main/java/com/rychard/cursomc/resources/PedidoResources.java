d018bfa2a012b4c6c1d4ecabc884f36f6d261c8a
Rychard souza <rychard34@outlook.com>
2019-01-10 18:59:08 -0300

    Criando profile de teste

--- src/main/java/com/rychard/cursomc/resources/PedidoResources.java
+++ src/main/java/com/rychard/cursomc/resources/PedidoResources.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.rychard.cursomc.domain.Categoria;
 import com.rychard.cursomc.domain.Pedido;
-import com.rychard.cursomc.dto.CategoriaDTO;
 import com.rychard.cursomc.services.PedidoService;
 
 @RestController
